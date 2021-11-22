467d8506e7535531b39368fbe064619bbdea44ad
Gematik <software-development@gematik.de>
2021-11-22 15:54:49 +0100

    Upgrade Gradle version

---
+++ package-search-index.js
@@ -0,0 +1,1 @@
+packageSearchIndex = [{"l":"All Packages","url":"allpackages-index.html"},{"l":"de.gematik.ti.epa.fdv.service.localization"}]
---
+++ type-search-index.js
@@ -0,0 +1,1 @@
+typeSearchIndex = [{"l":"All Classes","url":"allclasses-index.html"},{"p":"de.gematik.ti.epa.fdv.service.localization","l":"GatewayModulePathType"},{"p":"de.gematik.ti.epa.fdv.service.localization","l":"ServiceLocator"}]
